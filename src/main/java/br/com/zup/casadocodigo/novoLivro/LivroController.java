d742380232ca9528d9939b3e7051e51ebb9ea99d
DinizEduardo <eduardo.diniz987@gmail.com>
2021-02-17 19:28:09 -0300

    0-0-6-exibir-lista-livros -> Transactional read only

--- src/main/java/br/com/zup/casadocodigo/novoLivro/LivroController.java
+++ src/main/java/br/com/zup/casadocodigo/novoLivro/LivroController.java
@@ -1,12 +1,12 @@
 package br.com.zup.casadocodigo.novoLivro;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
-import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
@@ -31,7 +31,7 @@ public class LivroController {
     }
 
     @GetMapping
-    @Transactional
+    @Transactional(readOnly = true)
     public List<RetornoLivro> listar() {
         return manager.createQuery("select l from Livro l", Livro.class)
                 .getResultStream()
